5fdbe6e53b4089182d30fb0e70a9fca4993fa7ed
Dusan Punosevac <dusan.punosevac@gmail.com>
2025-02-08 16:49:01 +0100

    Refactor

---
+++ lua/custom/configs/conform.lua
@@ -0,0 +1,28 @@
+local conform = require("conform")
+
+conform.setup({
+    formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        svelte = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        lua = { "stylua" },
+        python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+        go = { "gofumpt", "goimports-reviser", "golines" }
+    },
+    formatters = {
+        ["goimports-reviser"] = { prepend_args = { "-rm-unused" } },
+        golines = { prepend_args = { "--max-len=100" } },
+    },
+    format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+    },
+})
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,59 +1,61 @@
 local plugins = {
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "gopls",
-      },
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "gopls",
+                "pyright",
+                "ruff",
+            },
+        },
     },
-  },
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      "nvim-neotest/nvim-nio",
-      "rcarriga/nvim-dap-ui",
-      "theHamsta/nvim-dap-virtual-text"
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "nvim-neotest/nvim-nio",
+            "rcarriga/nvim-dap-ui",
+            "theHamsta/nvim-dap-virtual-text"
+        },
+        init = function()
+            require("core.utils").load_mappings("dap")
+        end,
+        config = function()
+            require "custom.configs.debbug"
+        end
     },
-    init = function()
-      require("core.utils").load_mappings("dap")
-    end,
-    config = function ()
-      require "custom.configs.debbug"
-    end
-  },
-  {
-    "leoluz/nvim-dap-go",
-    ft = "go",
-    dependencies = "mfussenegger/nvim-dap",
-    config = function(_, opts)
-      require("dap-go").setup(opts)
-      require("core.utils").load_mappings("dap_go")
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    ft = "go",
-    opts = function()
-      return require "custom.configs.null-ls"
-    end,
-  },
-  {
-    "olexsmir/gopher.nvim",
-    ft = "go",
-    config = function(_, opts)
-      require("gopher").setup(opts)
-      require("core.utils").load_mappings("gopher")
-    end,
-    build = function()
-      vim.cmd [[silent! GoInstallDeps]]
-    end,
-  },
+    {
+        "leoluz/nvim-dap-go",
+        ft = "go",
+        dependencies = "mfussenegger/nvim-dap",
+        config = function(_, opts)
+            require("dap-go").setup(opts)
+            require("core.utils").load_mappings("dap_go")
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "plugins.configs.lspconfig"
+            require "custom.configs.lspconfig"
+        end,
+    },
+    {
+        "stevearc/conform.nvim",
+        event = { "BufWritePre" },
+        config = function()
+            require "custom.configs.conform"
+        end,
+    },
+    {
+        "olexsmir/gopher.nvim",
+        ft = "go",
+        config = function(_, opts)
+            require("gopher").setup(opts)
+            require("core.utils").load_mappings("gopher")
+        end,
+        build = function()
+            vim.cmd [[silent! GoInstallDeps]]
+        end,
+    },
 }
 return plugins
