0e4730257e901b7b8b63c160ed1ffaaae58784cd
Dusan Punosevac <dusan.punosevac@rho.co>
2025-06-17 11:56:43 +0200

    Add custom full path on statusline

--- lua/core/default_config.lua
+++ lua/core/default_config.lua
@@ -1,89 +1,111 @@
 local M = {}
 
 M.options = {
-  nvchad_branch = "v2.0",
+    nvchad_branch = "v2.0",
 }
 
 M.ui = {
-  ------------------------------- base46 -------------------------------------
-  -- hl = highlights
-  hl_add = {},
-  hl_override = {},
-  changed_themes = {},
-  theme_toggle = { "onedark", "one_light" },
-  theme = "onedark", -- default theme
-  transparency = false,
-  lsp_semantic_tokens = false, -- needs nvim v0.9, just adds highlight groups for lsp semantic tokens
+    ------------------------------- base46 -------------------------------------
+    -- hl = highlights
+    hl_add = {},
+    hl_override = {},
+    changed_themes = {},
+    theme_toggle = { "onedark", "one_light" },
+    theme = "onedark",           -- default theme
+    transparency = false,
+    lsp_semantic_tokens = false, -- needs nvim v0.9, just adds highlight groups for lsp semantic tokens
 
-  -- https://github.com/NvChad/base46/tree/v2.0/lua/base46/extended_integrations
-  extended_integrations = {}, -- these aren't compiled by default, ex: "alpha", "notify"
+    -- https://github.com/NvChad/base46/tree/v2.0/lua/base46/extended_integrations
+    extended_integrations = {}, -- these aren't compiled by default, ex: "alpha", "notify"
 
-  -- cmp themeing
-  cmp = {
-    icons = true,
-    lspkind_text = true,
-    style = "default", -- default/flat_light/flat_dark/atom/atom_colored
-    border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
-    selected_item_bg = "colored", -- colored / simple
-  },
-
-  telescope = { style = "borderless" }, -- borderless / bordered
-
-  ------------------------------- nvchad_ui modules -----------------------------
-  statusline = {
-    theme = "default", -- default/vscode/vscode_colored/minimal
-    -- default/round/block/arrow separators work only for default statusline theme
-    -- round and block will work for minimal theme only
-    separator_style = "default",
-    overriden_modules = nil,
-  },
-
-  -- lazyload it when there are 1+ buffers
-  tabufline = {
-    show_numbers = false,
-    enabled = true,
-    lazyload = true,
-    overriden_modules = nil,
-  },
-
-  -- nvdash (dashboard)
-  nvdash = {
-    load_on_startup = false,
-
-    header = {
-      "           ▄ ▄                   ",
-      "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
-      "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
-      "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
-      "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
-      "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
-      "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
-      "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
-      "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
+    -- cmp themeing
+    cmp = {
+        icons = true,
+        lspkind_text = true,
+        style = "default",            -- default/flat_light/flat_dark/atom/atom_colored
+        border_color = "grey_fg",     -- only applicable for "default" style, use color names from base30 variables
+        selected_item_bg = "colored", -- colored / simple
     },
 
-    buttons = {
-      { "  Find File", "Spc f f", "Telescope find_files" },
-      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
-      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
-      { "  Bookmarks", "Spc m a", "Telescope marks" },
-      { "  Themes", "Spc t h", "Telescope themes" },
-      { "  Mappings", "Spc c h", "NvCheatsheet" },
-    },
-  },
+    telescope = { style = "borderless" }, -- borderless / bordered
 
-  cheatsheet = { theme = "grid" }, -- simple/grid
+    ------------------------------- nvchad_ui modules -----------------------------
+    statusline = {
+        theme = "default", -- default/vscode/vscode_colored/minimal
+        -- default/round/block/arrow separators work only for default statusline theme
+        -- round and block will work for minimal theme only
+        separator_style = "default",
+        overriden_modules = function(modules)
+            -- Get the file path (absolute)
+            local filepath = vim.fn.expand("%:p")
+            -- Get the file extension
+            local ext = vim.fn.expand("%:e")
+            -- Get the file icon and icon highlight group
+            local icon = ""
+            local icon_hl = "St_file_icon"
+            local devicons_present, devicons = pcall(require, "nvim-web-devicons")
+            if devicons_present then
+                local ft_icon, hl = devicons.get_icon(filepath, ext, { default = true })
+                if ft_icon then
+                    icon = ft_icon
+                end
+                if hl then
+                    icon_hl = hl
+                end
+            end
 
-  lsp = {
-    -- show function signatures i.e args as you type
-    signature = {
-      disabled = false,
-      silent = true, -- silences 'no signature help available' message from appearing
+            -- Compose the module with highlights (using NvChad's highlight groups)
+            modules[2] = string.format("%%#%s# %s %%#St_file# %s ", icon_hl, icon, filepath)
+            return modules
+        end,
     },
-  },
+
+    -- lazyload it when there are 1+ buffers
+    tabufline = {
+        show_numbers = false,
+        enabled = true,
+        lazyload = true,
+        overriden_modules = nil,
+    },
+
+    -- nvdash (dashboard)
+    nvdash = {
+        load_on_startup = false,
+
+        header = {
+            "           ▄ ▄                   ",
+            "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
+            "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
+            "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
+            "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
+            "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
+            "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
+            "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
+            "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
+        },
+
+        buttons = {
+            { "  Find File", "Spc f f", "Telescope find_files" },
+            { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+            { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+            { "  Bookmarks", "Spc m a", "Telescope marks" },
+            { "  Themes", "Spc t h", "Telescope themes" },
+            { "  Mappings", "Spc c h", "NvCheatsheet" },
+        },
+    },
+
+    cheatsheet = { theme = "grid" }, -- simple/grid
+
+    lsp = {
+        -- show function signatures i.e args as you type
+        signature = {
+            disabled = false,
+            silent = true, -- silences 'no signature help available' message from appearing
+        },
+    },
 }
 
-M.plugins = "" -- path i.e "custom.plugins", so make custom/plugins.lua file
+M.plugins = ""                                    -- path i.e "custom.plugins", so make custom/plugins.lua file
 
 M.lazy_nvim = require "plugins.configs.lazy_nvim" -- config for lazy.nvim startup options
 
